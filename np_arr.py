b3a3f9e1820c5730a080c22bf6fe770640cd71d5
khushi-paneliya <paneliyakhushi777@gmail.com>
2026-07-10 09:16:11 +0530

    Add files via upload

---
+++ np_arr.py
@@ -0,0 +1,22 @@
+import sys
+import numpy as np
+arr1=np.array([10,20,30,40,50])
+print(arr1)
+print(arr1+10)
+print(arr1+2)
+print(arr1-30)
+
+print("sum of array")
+arr2=np.array([5,4,3,2,1])
+print(arr1+arr2)
+
+
+print("min of array")
+print(arr1-arr2)
+
+print("multiplication of array")
+print(arr1*arr2)
+
+print("division of array")
+print(arr1/arr2)
+
