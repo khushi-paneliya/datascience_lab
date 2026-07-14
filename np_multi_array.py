82afbc9ec3d638544fee29bf346e97dfe531e9b3
khushi-paneliya <paneliyakhushi777@gmail.com>
2026-07-14 08:37:06 +0530

    Add files via upload

---
+++ np_multi_array.py
@@ -0,0 +1,15 @@
+import sys
+import numpy as np
+
+arr=np.array([[10,20,30,40],[5,6,7,8]])
+print(arr)
+print(arr[0,0])
+print(arr[0,1])
+print(arr[0,2])
+print(arr[0,3])
+
+
+print(arr[1,0])
+print(arr[1,1])
+print(arr[1,2])
+print(arr[1,3])
