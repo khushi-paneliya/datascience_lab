d5e1df5e40426d2190094c83b92d4fd34a44c903
khushi-paneliya <paneliyakhushi777@gmail.com>
2026-07-31 09:13:49 +0530

    Add files via upload

---
+++ linechart1.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+df=pd.read_csv("sales_data.csv")
+plt.plot(df["Month"],df["Sales"],marker='o')
+plt.title("Monthly Sales")
+plt.xlabel("Month")
+plt.ylabel("Sales")
+plt.show()
